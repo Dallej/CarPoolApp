a92e263de6df4307ef961eb3fc6c69830bd9ecaf
Dallej <57659831+Dallej@users.noreply.github.com>
2021-03-24 13:11:22 +0200

    app.js changes

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './style/style'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>CARPOOL APP</Text>
-      <StatusBar style="auto" />
+      <View style={styles.main}>
+        <View>
+          <image src="./img/OAMKlogo"></image>
+        </View>
+
+        <View>
+          <button onPress={() => navigation.navigate('Rides')}>
+            Rides
+          </button>
+        </View>
+        <button onPress={() => navigation.navigate('OfferARide')}>
+          Offer a ride +
+        </button>
+        <View>
+          <button onPress={() => navigation.navigate('Info')}>
+            Instructions
+          </button>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
