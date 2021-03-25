37e24d52a36342c93109c2204fd880fc0ffe56b2
elise laurila <n9lael00@students.oamk.fi>
2021-03-25 11:49:13 +0200

    sidebar & hamburger

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import {Text, View , Image, Button,Link } from 'react-native';
+import {Text, View , Image, Button,Link, Menu, Navigator } from 'react-native';
 import styles from './style/style';
-
+import Sidebar from './Components/Sidebar';
 
 export default function App() {
+
+      
   return (
+
     <View style={styles.container}>
+
       <View style={styles.main}>
-        <View >
-        <Image source={require("./img/OAMKlogo.png")} style={styles.logo} />
-        </View>
-        {/* <Link to="./Info" className="btn btn-primary">info</Link> */}
-        <View>
-          <Button style={styles.button} onPress={() => navigation.navigate('Rides')}>
-            Rides
-          </Button>
-        </View>
-        <Button style={styles.button} onPress={() => navigation.navigate('OfferARide')}>
-          Offer a ride +
-        </Button>
-        <View>
-          <Button style={styles.button} onPress={() => navigation.navigate('Info')}>
-            Instructions
-          </Button>
-        </View>
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       </View>
     </View>
   );
