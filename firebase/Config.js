aaa990dad8f35d467f56fbe61e1824e469426e6d
Jjohannesaf <n9ahjo00@students.oamk.fi>
2021-03-25 12:05:30 +0200

    config
    fb

--- firebase/Config.js
+++ firebase/Config.js
@@ -7,8 +7,10 @@ const firebaseConfig = {
     projectId: "carpool-app-bc6e0",
     storageBucket: "carpool-app-bc6e0.appspot.com",
     messagingSenderId: "12710615674",
-    appId: "1:12710615674:web:0712629791d5cbfb4539c3"
+    appId: "1:12710615674:web:0712629791d5cbfb4539c3",
+    databaseURL: "https://carpool-app-bc6e0-default-rtdb.firebaseio.com/"
   };
  
   const app = firebase.initializeApp(firebaseConfig);
   export const db = app.database();
+  export const ROOT_REF='//';
